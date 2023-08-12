16a690586570093a778b977140c51dce3506c268
BrianEC18 <132420037+BrianEC18@users.noreply.github.com>
2023-08-12 16:29:06 -0600

    Modificación de los archivos para añadir descripciónes

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// index.js: Manejo de enlaces y cierre de sesión en la página principal
+
+// Obtener elementos de los iconos de enlace
 const linkButtons = document.querySelectorAll(".icons");
 const logoutButton = document.querySelector(".logout-button");
 
+// Agregar eventos a los iconos de enlace
 linkButtons.forEach(link => {
     link.addEventListener("click", function (e) {
+        // Prevenir comportamiento predeterminado
         e.preventDefault();
 
-        const getLink = this.parentElement.getAttribute("href");
-        navigator.clipboard.writeText(getLink)
+        // Obtener enlace y copiarlo al portapapeles
+        const linkURL = this.parentElement.getAttribute("href");
+        navigator.clipboard.writeText(linkURL)
             .then(() => {
                 alert("Enlace copiado en el portapapeles");
             })
@@ -16,11 +22,18 @@ linkButtons.forEach(link => {
     });
 });
 
+// Agregar evento al botón de cierre de sesión
 logoutButton.addEventListener("click", function () {
+    // Confirmar cierre de sesión
     const confirmLogout = confirm("¿Estás seguro de que deseas cerrar sesión?");
+    
+    // Si el usuario confirma el cierre de sesión
     if (confirmLogout) {
+        // Eliminar datos de inicio de sesión almacenados en localStorage
         localStorage.removeItem("loggedIn");
         localStorage.removeItem("loginTime");
+        
+        // Redirigir a la página de inicio de sesión
         window.location.href = "login.html";
     }
 });
--- login.js
+++ login.js
@@ -1,20 +1,30 @@
+// login.js: Manejo de inicio de sesión y validación en la página de inicio de sesión
+
+// Esperar a que el DOM esté cargado
 document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el formulario y elementos dentro del formulario
     const form = document.getElementById("login-form");
     const usernameInput = form.querySelector("input[type='text']");
     const passwordInput = form.querySelector("input[type='password']");
-    const rememberCheckbox = form.querySelector("input[type='checkbox']");
     const submitButton = form.querySelector(".btn");
 
+    // Agregar evento al enviar el formulario
     form.addEventListener("submit", function (event) {
+        // Prevenir el envío predeterminado del formulario
         event.preventDefault();
         
+        // Obtener el valor del nombre de usuario y contraseña
         const username = usernameInput.value;
         const password = passwordInput.value;
 
+        // Verificar las credenciales ingresadas
         if (username === "Admin" && password === "123456") {
+            // Si las credenciales son correctas, redirigir a la página principal
             window.location.href = "index.html";
         } else {
+            // Si las credenciales son incorrectas, mostrar una alerta
             alert("Usuario o contraseña incorrectos.");
         }
     });
 });
+
